a272763a8cee8f03eb5de273969adf66fcc2c711
NicholasFields <68744844+NicholasFields@users.noreply.github.com>
2023-03-11 22:59:59 +0000

    add domain to prod deployment

--- experiments/zave-portfolio/stacks/Default.ts
+++ experiments/zave-portfolio/stacks/Default.ts
@@ -1,10 +1,21 @@
-import { StackContext, NextjsSite } from "sst/constructs";
+import { StackContext, NextjsSite, use } from "sst/constructs";
+import { Dns } from "./Dns";
 
-export function Default({ stack }: StackContext) {
+export function Default({ stack, app }: StackContext) {
+  const { hostedZone } = use(Dns);
+
+  const customDomain = app.stage === "prod" ? {
+    domainName: hostedZone?.zoneName as string,
+    domainAlias: `www.${hostedZone?.zoneName}`,
+  } : undefined;
+
   const site = new NextjsSite(stack, "site", {
     path: "packages/next",
+    customDomain,
   });
+
   stack.addOutputs({
     ApiEndpoint: site.url || "http://localhost:3000",
   });
 }
+
