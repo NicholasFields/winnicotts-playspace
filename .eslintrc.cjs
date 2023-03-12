fab9951a8389354a9ad4afa05bf03dd453a3cf8d
NicholasFields <68744844+NicholasFields@users.noreply.github.com>
2023-03-12 06:21:19 +0000

    tweaking eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -39,5 +39,21 @@ module.exports = {
 			'always'
 		],
 		'react/jsx-max-props-per-line': [1, { 'maximum': 1, 'when': 'always' }],
+		'react/jsx-first-prop-new-line': [1, 'multiline'],
+		'react/jsx-indent': [1, 'tab'],
+		'react/jsx-indent-props': [1, 'tab'],
+		'react/jsx-closing-bracket-location': [1, 'line-aligned'],
+		'react/jsx-closing-tag-location': 1,
+		'react/jsx-curly-spacing': [1, 'always'],
+		'react/jsx-equals-spacing': [1, 'never'],
+		'react/jsx-tag-spacing': [1, { 'beforeSelfClosing': 'always' }],
+		'react/jsx-wrap-multilines': [1, { 'declaration': 'parens-new-line', 'assignment': 'parens-new-line', 'return': 'parens-new-line', 'arrow': 'parens-new-line', 'condition': 'parens-new-line', 'logical': 'parens-new-line', 'prop': 'parens-new-line' }],
+		'react/jsx-curly-newline': [1, { 'multiline': 'consistent', 'singleline': 'consistent' }],
+		'react/jsx-props-no-multi-spaces': 1,
+		'react/jsx-one-expression-per-line': 0,
+		'react/jsx-child-element-spacing': 0,
+		'no-multi-spaces': 1,
+		'react/jsx-fragments': 0,
+	
 	}
 };
--- experiments/zave-portfolio/packages/next/app/page.tsx
+++ experiments/zave-portfolio/packages/next/app/page.tsx
@@ -1,33 +1,47 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const Header = () => {
+	//
+	return 			(
+		<head>
+			<title>Json Doe</title>
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta
+				property="og:title"
+				content="nick/portfolio"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="./images/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="./images/favicon-16x16.png"
+			/>
+			<link
+				rel="stylesheet"
+				href="./styles/index.css"
+			/>
+			<link
+				rel="stylesheet"
+				href="./styles/navbar.css"
+			/>
+		</head>
+	);
+};
 
 
 export default function Home() {
 	return (
 		<>
-			<head>
-				<title>Json Doe</title>
-				<meta name="viewport"
-					content="width=device-width, initial-scale=1.0" />
-
-				<meta property="og:title"
-					content="nick/portfolio" />
-				<link rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="./images/favicon-32x32.png" />
-				<link rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="./images/favicon-16x16.png" />
-				<link rel="stylesheet"
-					href="./styles/index.css" />
-				<link rel="stylesheet"
-					href="./styles/navbar.css" />
-			</head>
+			<Header />
 			<body>
 				<div id="scripted-page">
 					<header id="main-navbar">
@@ -35,15 +49,25 @@ export default function Home() {
 							<span id="mini-navbar-root">~/</span><span id="full-navbar-root">nick/portfolio/</span>
 							<span id="current-location">nick</span>
 						</span>
-						<span id="navbar-command"
-							className="navbar-start-hidden">
-							<span id="full-navbar-command"
-								className="navbar-command-text">&nbsp;navigate&nbsp;</span>
-							<span id="mini-navbar-command"
-								className="navbar-command-text">&nbsp;nav&nbsp;</span>
+						<span
+							id="navbar-command"
+							className="navbar-start-hidden"
+						>
+							<span
+								id="full-navbar-command"
+								className="navbar-command-text"
+							>&nbsp;navigate&nbsp;
+							</span>
+							<span
+								id="mini-navbar-command"
+								className="navbar-command-text"
+							>&nbsp;nav&nbsp;
+							</span>
 						</span>
-						<nav id="navbar-links"
-							className="navbar-start-hidden">
+						<nav
+							id="navbar-links"
+							className="navbar-start-hidden"
+						>
 							<ul>
 								<li id="home-link">
 									<a href="#home">home</a>
@@ -64,27 +88,43 @@ export default function Home() {
 					<script src="./scripts/navbar.js"></script>
 
 					<div id="page-content">
-						<section id="home"
-							className="active-section">
+						<section
+							id="home"
+							className="active-section"
+						>
 							<span className="wordart-header type-contents">{'hello'}
-							</span><span id="name-header"
-								className="wordart-header type-contents">nametext</span>
+							</span><span
+								id="name-header"
+								className="wordart-header type-contents"
+							>nametext
+							</span>
 							<p className="type-contents">(fullstack node.js developer)</p>
 							<nav className="link-list">
-								<a className="type-contents"
-									href="#about">[learn about me]</a>
-								<a className="type-contents"
-									href="#projects">[cool projects]</a>
-								<a className="type-contents"
-									href="#contact">[chat with me]</a>
+								<a
+									className="type-contents"
+									href="#about"
+								>[learn about me]
+								</a>
+								<a
+									className="type-contents"
+									href="#projects"
+								>[cool projects]
+								</a>
+								<a
+									className="type-contents"
+									href="#contact"
+								>[chat with me]
+								</a>
 							</nav>
 							<noscript>
 								<br />
-								<p style={{ fontSize: '0.7rem' }}>pssst.. this page is better with javascript enabled</p>
+								<p style={ { fontSize: '0.7rem' } }>pssst.. this page is better with javascript enabled</p>
 							</noscript>
 						</section>
-						<section id="about"
-							className="active-section">
+						<section
+							id="about"
+							className="active-section"
+						>
 							<h1 className="wordart-header type-contents">about</h1>
 							<br />
 							<h2><span className="type-contents">hi, im</span> <span className="type-contents name">nick</span></h2>
@@ -92,53 +132,76 @@ export default function Home() {
 							<p className="about-paragraph type-contents">description stuff</p>
 							<p className="about-paragraph type-contents">description stuff</p>
 						</section>
-						<section id="projects"
-							className="active-section">
+						<section
+							id="projects"
+							className="active-section"
+						>
 							<h1 className="wordart-header type-contents">projects</h1>
 							<br />
 							<table id="projects-table">
 								<tr>
 									<td className="type-contents">proj name</td>
 									<td className="type-contents">proj desc</td>
-									<td><a className="github-link type-contents"
+									<td><a
+										className="github-link type-contents"
 										href="https://github.com/NicholasFields/proj-name"
 										target="_blank"
-										rel="noreferrer">[github]</a></td>
+										rel="noreferrer"
+									>[github]
+									</a>
+									</td>
 								</tr>
 							</table>
 						</section>
-						<section id="contact"
-							className="active-section">
+						<section
+							id="contact"
+							className="active-section"
+						>
 							<h1 className="wordart-header type-contents">contact-ascii</h1>
 							<nav>
 								<br />
 								<table id="contact-info">
 									<tr>
-										<td><a className="type-contents bracket-link"
+										<td><a
+											className="type-contents bracket-link"
 											href="mailto:nick@jsondoe.com"
 											target="_blank"
-											style={{ color: '#6D4AFF' }}
-											rel="noreferrer">[email]</a></td>
+											style={ { color: '#6D4AFF' } }
+											rel="noreferrer"
+										>[email]
+										</a>
+										</td>
 										<td className="type-contents">nick@jsondoe.com</td>
 									</tr>
 									<tr>
-										<td><a className="type-contents"
+										<td><a
+											className="type-contents"
 											href="https://twitter.com/twitterhandle"
 											target="_blank"
-											style={{ color: '#1D9BF0' }}
-											rel="noreferrer">[twitter]</a></td>
+											style={ { color: '#1D9BF0' } }
+											rel="noreferrer"
+										>[twitter]
+										</a>
+										</td>
 										<td className="type-contents">@twitterhandle</td>
 									</tr>
 									<tr>
-										<td className="type-contents"
-											style={{ color: '#454FBF ' }}>[discord]</td>
+										<td
+											className="type-contents"
+											style={ { color: '#454FBF ' } }
+										>[discord]
+										</td>
 										<td className="type-contents">discordhandle</td>
 									</tr>
 									<tr>
-										<td><a className="type-contents github-link"
+										<td><a
+											className="type-contents github-link"
 											href="https://github.com/NicholasFields"
 											target="_blank"
-											rel="noreferrer">[github]</a></td>
+											rel="noreferrer"
+										>[github]
+										</a>
+										</td>
 										<td className="type-contents">@githubhandle</td>
 									</tr>
 								</table>
@@ -146,7 +209,8 @@ export default function Home() {
 						</section>
 						<section id="404">
 							<h1 className="wordart-header type-contents">
-                404</h1>
+								404
+							</h1>
 							<p className="type-contents">Huh, this isn&apos;t an official page on this site, why aren&apos;t you using the navbar?</p>
 						</section>
 					</div>
