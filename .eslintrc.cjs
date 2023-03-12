75d4428ec6de622f78004d6f25b6d11bbd9f70db
NicholasFields <68744844+NicholasFields@users.noreply.github.com>
2023-03-12 20:58:15 +0000

    tinkering with linting -- where is the effortless insta-setup?

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,44 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "standard-with-typescript",
-        "prettier" 
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['simple-import-sort'],
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "plugins": [
-        "prettier",
-        "react",
-
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:security/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'no-console': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'simple-import-sort/sort': 'error',
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          camelCase: true,
+        },
+      },
     ],
-}
+  },
+};
